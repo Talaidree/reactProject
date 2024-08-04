67b1a2248444b921e027409f672a3ae921e73982
Talaidree <211153@ppu.edu.ps>
2024-08-04 20:01:53 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Footer from './footer';
+
+function App() {
+  return (
+    <div className="App">
+      
+      
+      <header/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
